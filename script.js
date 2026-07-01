a0ed17ba0cadc6550be6a467a427d2a511d86712
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-01 14:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 //complete this code
 class Rectangle {
 	constructor(width, height){
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 	}
 
 	get returnWidth(){
-		return this.width;
+		return this._width;
 	}
 
 	get returnHeight(){
-		return this.height;
+		return this._height;
 	}
 
 	getArea(){
-		return (this.width*this.height);
+		return (this._width*this._height);
 	}
 }
 
 class Square extends Rectangle {
-	constructor(width, height, getArea, side){
-		super(width,height,getArea);
-		this.side = side;
-	}
-
-	get returnWidht(){
-		this.width = side;
-	}
-
-	get returnHeight(){
-		this.height = side;
-	}
-
-	getArea(){
-		return this.side*this.side;
+	constructor(side){
+		super(side, side);
 	}
 
 	getPerimeter(){
-		return 4*this.side;
+		return 4*this.width;
 	}
 	
 }
